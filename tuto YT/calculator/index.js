14a71326a2f59637ff6ba250568a2e736303b30a
unknown <audricrosier@gmail.com>
2021-10-13 19:01:55 +0200

    Update index.js
    - add Keyboard control

--- tuto YT/calculator/index.js
+++ tuto YT/calculator/index.js
@@ -15,3 +15,41 @@ equal.addEventListener('click', () =>{
 clear.addEventListener('click', () => {
   result.textContent = "";
 })
+
+window.onkeydown = function (key) {
+  switch (key['key']) {
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+    case "0":
+
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+    case ".":
+    case "(":
+    case ")":
+      
+      result.textContent += key['key'];
+      break;
+
+    case 'Enter':
+      result.textContent = eval(result.textContent);
+      break;
+    
+    case 'Backspace':
+      result.textContent = result.textContent.substring(0, result.textContent.length - 1);
+      break;
+
+    default:
+      //console.log(key['key'])
+      break;
+  }
+}
